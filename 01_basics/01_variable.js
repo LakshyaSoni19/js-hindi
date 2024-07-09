78c8baf0616fb9989b54c3dab6d4cef84bcc6a33
LakshyaSoni19 <lakshyasoni.19official@gmail.com>
2024-07-09 09:31:27 +0000

    container configure done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId=14453
+let accountEmail = "laks@gmail.com"
+var accountPassword = "1234"
+accountCity = "Indore" 
+
+// accountId=541545
+
+console.log(accountId);
+accountEmail = "soni@gmail.com"
+accountPassword="65656"
+accountCity="delhi"
+console.table([accountEmail,accountPassword,accountCity])
