2aaa261c601aa945a4e87e19b85b371bd386cea3
LakshyaSoni19 <lakshyasoni.19official@gmail.com>
2024-07-10 05:21:31 +0000

    container config done

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,29 @@
+// let score = "33 abc"
+// let score = "33 abc"
+
+// let score = "true"
+
+let score = "undefined"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIN =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIN)
+
+console.log(booleanIsLoggedIn)
+
+let someNumber =33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+
+// node 01_basics/03_conversion.js
