d7cf74f48962e9c1dcfeb64cdc83eaf00d4201ad
LakshyaSoni19 <lakshyasoni.19official@gmail.com>
2024-07-10 04:54:07 +0000

    container config done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+
+let name = "lakshya"
+let age = 21
+let isLoggedIn = false
+let state;
+
+ /*number
+ bigint
+ string
+ boolean
+ null
+ undefined
+ symbol
+ 
+ object
+ 
+ */ 
+console.log("name")
+
+console.log(typeof undefined)
+
+console.log(typeof null)
