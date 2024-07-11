9c1ccb8f8f102171566581f871aa9224df3b7c46
LakshyaSoni19 <lakshyasoni.19official@gmail.com>
2024-07-11 07:57:27 +0000

    container config done

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -3,27 +3,59 @@
 
 // let score = "true"
 
-let score = "undefined"
+// let score = "undefined"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-let isLoggedIN =1
+// let isLoggedIN =1
 
-let booleanIsLoggedIn = Boolean(isLoggedIN)
+// let booleanIsLoggedIn = Boolean(isLoggedIN)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-let someNumber =33
-let stringNumber = String(someNumber)
+// let someNumber =33
+// let stringNumber = String(someNumber)
 
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *******OPERATION********
+
+
+let value = 3
+ let negValue = -value
+//  console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+// _________________________________________
+
+
+// let str1 = "hello"
+// let str2 = " Lakshya"
+// let Str3 = str1+ str2
+// console.log(Str3)
+ 
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+
+
+
+
+
+
 
 
 // node 01_basics/03_conversion.js
