8097c78df43623ecffd522a3846d69b949961506
LakshyaSoni19 <lakshyasoni.19official@gmail.com>
2024-07-12 18:26:00 +0000

    container config done

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,45 @@
+// Primitive
+
+// String , Number, Boolean , Null , undefined,Symbol BigInt
+
+const score = 100
+const scoreVAlue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId)
+
+const bigNumber = 9559845463998918685n
+console.log(typeof bigNumber)
+
+// Non primitive datatype
+
+// Array , object ,functions
+
+
+
+const heros = ["Shaktiman", "naagraj","doga"]
+
+let myObj = {
+    name: "lakshya",
+    age:21
+
+}
+const myFunction = function(){
+    console.log("hello World");
+}
+
+
+
+
+
+
+
+
+
+
