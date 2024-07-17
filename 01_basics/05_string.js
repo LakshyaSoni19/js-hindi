137d6c5d8638fbb768a7aa17576acc51e78974d4
LakshyaSoni19 <lakshyasoni.19official@gmail.com>
2024-07-17 17:45:02 +0000

    container config done

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,3 +5,33 @@ const repoCount = 50
 // Don't use above
 
 console.log(` Hello my name is ${name} and my repo is ${repoCount}`);
+
+const getName = new String('jethalal')
+
+console.log(getName[0]);
+console.log(getName.__proto__);
+
+
+
+console.log(getName.toUpperCase());
+console.log(getName.charAt(2));
+console.log(getName.indexOf('t'));
+
+const newString = getName.substring(-9,4)
+console.log(newString);
+
+const anotherString = getName.slice(-8,4)
+console.log(anotherString);
+
+
+const newStringOne = "     Babita"
+console.log(newStringOne);
+console.log(newString.trim());
+
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20','_'));
+
+console.log(getName.split('-'));
