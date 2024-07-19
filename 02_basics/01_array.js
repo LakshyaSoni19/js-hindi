79872571bcaa485bc60fc97a9fb177e3c537785e
LakshyaSoni19 <lakshyasoni.19official@gmail.com>
2024-07-19 04:27:56 +0000

    container config done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,26 @@
+// Arrays
+
+
+const myArr =[1,2,3,4,5,true,"Lakshya"];
+
+const myHero = ["tapu","Sonu","gogi ","goli"];
+
+const myArr2 = new Array(0,9,8,7)
+
+// console.log(myArr[0]);
+// console.log(myHero[1]);
+
+// Array Methods
+
+// myArr.push(6,7)
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+
+myArr.unshift();
+console.log(myArr);
+
+
+
+
