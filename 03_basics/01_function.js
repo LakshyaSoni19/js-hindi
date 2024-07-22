5fc086920d470bef3d12c65e70d353ceb57dfde6
LakshyaSoni19 <lakshyasoni.19official@gmail.com>
2024-07-22 18:08:16 +0000

    container config done

---
+++ 03_basics/01_function.js
@@ -0,0 +1,37 @@
+function Name(){
+    console.log("L");
+    console.log("a");
+    console.log("k");
+    console.log("s");
+    console.log("h");
+    console.log("y");
+    console.log("a");
+}
+// Name()
+
+// function add(num1,num2){
+//     console.log(num1+num2);
+// }
+
+
+function add(num1,num2){
+   const result = num1+num2
+   return result 
+   console.log("laksya")
+}
+// add(3,4)
+// const result = add(3,4)
+// console.log("Result",result)
+
+function loginUserMessage(username)
+{if(username === undefined){
+    console.log("please enter a username")
+    return 
+}
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage("Lakshya")
+
+console.log(loginUserMessage())
+
+
