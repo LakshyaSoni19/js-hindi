2cc4d36f0837766b7062b600ec04ab4bcbca8dba
LakshyaSoni19 <lakshyasoni.19official@gmail.com>
2024-07-31 06:35:39 +0000

    container config done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,45 @@
+// if else
+// const isUserLoggedIn = true;
+// const temperature = 41;
+
+// if(temperature<45){
+
+// console.log("temperature is less than 45");
+// }else{
+// console.log("temperature is Greater than 45");
+
+// }
+
+// const score = 200
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+// const  balance = 1000;
+
+// // if (balance > 500) console.log("test"),console.log("test");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// }else if (balance < 750) {
+//     console.log("less than 750");
+// }else if (balance < 900) {
+//     console.log("less than 900");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+// const isUserLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if (isUserLoggedIn && debitCard) {
+//     console.log("Allow user to log in..");
+// }
+// if (loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("Allow user to log in..");
+// }
+
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,41 @@
+// const userEmail = "Lask@gmail.com";
+
+// if (userEmail) {
+//   console.log("Got user email");
+// } else {
+//   console.log("Dont't have user email");
+// }
+
+// const userEmail =[]
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+// const emptyObj ={}
+
+// if (Object.keys(emptyObj).length === 0) {
+//   console.log("Object is empty");  
+// }
+
+
+// nullish coalescing Operator (??):  Null Undefined 
+
+let val1 ;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+ 
+// val1 = undefined ?? 15
+
+// val1 = null ?? 10 ?? 20 
+
+
+// console.log(val1);
+
+// *******ternary Operator*******
+
+// Condition ? true:false
+
+// const iceTeaPrice = 100 
+
+// iceTeaPrice>=80 ? console.log("less than 80"):console.log("More than 80");
+
