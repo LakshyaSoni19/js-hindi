601f2799ca435539540f1fce0c4033b1fc870eb7
LakshyaSoni19 <lakshyasoni.19official@gmail.com>
2024-08-01 04:40:42 +0000

    container config done

--- 05_basics/one.js
+++ 05_basics/one.js
@@ -1,45 +1,45 @@
 //for
 for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if (element == 5) {
-      // console.log("5 is best number");
-    }
-    //   console.log(element);
+  const element = i;
+  if (element == 5) {
+    // console.log("5 is best number");
   }
-  
-  // for (let i = 1; i <= 10; i++) {
-  //   console.log(`Outer loop: ${i}`);
-  
-  //   for (let j = 0; j <= 10; j++) {
-  //     // console.log(`Inner loop: ${j} and inner loop ${i}`);
-  
-  //     console.log(1 + `X` + j + `=` + i * j);
-  //   }
-  // }
-  
-  // let myArray = ["jethala", "tapu", "Daya"]
-  // for (let index = 0; index < myArray.length; index++) {
-  //   const element = myArray[index];
   //   console.log(element);
-  // }
-  
-  //  ************break and continue**************
-  
-  // for (let index = 1; index <= 20; index++) {
-  //   if (index == 5) {
-  //     console.log("5 mil gaya");
-  //     break;
-  //   }
-  //   console.log(`Value of i is :${index}`);
-  // }
-  
-  
-  
-  
-  // for (let index = 1; index <= 20; index++) {
-  //     if (index == 5) {
-  //       console.log("5 mil gaya");
-  //   continue
-  //     }
-  //     console.log(`Value of i is :${index}`);
-  //   }
+}
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`Outer loop: ${i}`);
+
+//   for (let j = 0; j <= 10; j++) {
+//     // console.log(`Inner loop: ${j} and inner loop ${i}`);
+
+//     console.log(1 + `X` + j + `=` + i * j);
+//   }
+// }
+
+// let myArray = ["jethala", "tapu", "Daya"]
+// for (let index = 0; index < myArray.length; index++) {
+//   const element = myArray[index];
+//   console.log(element);
+// }
+
+//  ************break and continue**************
+
+// for (let index = 1; index <= 20; index++) {
+//   if (index == 5) {
+//     console.log("5 mil gaya");
+//     break;
+//   }
+//   console.log(`Value of i is :${index}`);
+// }
+
+
+
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//       console.log("5 mil gaya");
+//   continue
+//     }
+//     console.log(`Value of i is :${index}`);
+//   }
